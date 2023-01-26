242cc2b22d02241bf0596c0a0ff5f1f9a0b651bc
yuki <yuki.utk8g@gmail.com>
2023-01-26 19:05:16 +0300

    add some classes for command handlers, TODO
    They are implemented in telethon only to speed up this part. It should be possible to make them work with other API too, but I am too lazy to do this.
    
    Probably should move them somewhere else...

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import util
+import telethon.events
+import telethon
 
 log = util.get_log("main")
 log.info("AlterPy")
+
+telethon_config = util.get_config("telethon_config.toml")
+api_id = telethon_config['api_id']
+api_hash = telethon_config['api_hash']
+bot_token = telethon_config['bot_token']
+client = telethon.TelegramClient("alterpy", api_id, api_hash)
+client.start(bot_token=bot_token)
+log.info("Started telethon instance")
+
+# load all CommandHandlers from external files
+
+@client.on(telethon.events.NewMessage)
+async def event_handler(event):
+    pass
--- util.py
+++ util.py
@@ -2,6 +2,11 @@ import pytomlpp
 import logging
 import traceback
 import time
+import datetime
+import typing
+
+import telethon.tl.custom
+import telethon.events
 
 
 def get_config(name):
@@ -28,7 +33,7 @@ def get_log(name="unknown"):
     ti = ti.split(" ")
     ti = "_".join(ti[1:3])
 
-    h_file = logging.FileHandler(f"log/logs_{ti}.log", encoding="utf-8")
+    h_file = logging.FileHandler(f"log/{name}.log", encoding="utf-8")
     h_file.setFormatter(logging_formatter)
     log.addHandler(h_file)
     # log.addHandler(logging_handler_stderr)
@@ -37,3 +42,66 @@ def get_log(name="unknown"):
 
 def log_fail(log, text):
     log.error(f"{text}\n{traceback.format_exc()}")
+
+
+
+class MessageInteractor(typing.NamedTuple):
+    message: telethon.tl.custom.message.Message
+
+    async def reply(self, text, media):  #send reply
+        try:
+            await self.message.reply(text=text, media=media)
+        except Exception as e:
+            log_fail(get_log("telethon message interactor"), "Could not reply")
+
+    async def respond(self, text, media):
+        try:
+            await self.message.respond(text=text, media=media)
+        except Exception as e:
+            log_fail(get_log("telethon message interactor"), "Could not respond")
+
+    async def delete(self):  #just delete message
+        try:
+            await self.message.delete()
+        except Exception as e:
+            await self.reply(f"Error occurred:\n```\n{traceback.format_exc()}\n```")  # TODO fix message
+
+
+class User:
+    def is_admin(self):  #check if in admins list
+        pass  # TODO
+    async def get_displayname(self):
+        pass  # TODO
+    def get_mention(self):
+        pass  # TODO
+
+
+class CommandMessage(typing.NamedTuple):
+    arg: str  # message text
+    media: typing.Any  # media if exist
+    time: datetime  # UTC time when sent
+    local_time: datetime  # UTC time wher recv
+    sender: User  # sender
+    reply_sender: User  # reply sender if applicable
+    # chat: Chat  # chat object --- unneeded for now
+    int_cur: MessageInteractor  # for current message
+    int_prev: MessageInteractor  # for attached reply
+
+    def CommandMessage(self, event: telethon.events.NewMessage):
+        """Construct CommandMessage from telethon NewMessage event"""
+        # TODO
+        pass
+
+
+class CommandHandler(typing.NamedTuple):
+    is_elevated: bool  # should a command be invoked only if user is admin
+    is_replaceable: bool  # should a matched command pattern be replaced with nothing
+    help_message: str  # short help about command
+    name: str  # command name
+    pattern: str  # regex pattern
+    author: str
+    version: int
+    handler_impl: typing.Callable[[CommandMessage], typing.Awaitable]
+
+    async def invoke(self, cm: CommandMessage):
+        await self.handler_impl(cm)
