ba3b8cda97e1e9659ca1121a62a8e43df1a6b9af
yuki <yuki.utk8g@gmail.com>
2023-01-27 17:27:30 +0300

    add support for named advice

--- commands/adv.py
+++ commands/adv.py
@@ -7,26 +7,32 @@ import json
 handlers = []
 
 
-async def advice():
-    adv_json = requests.get(
-        'http://fucking-great-advice.ru/api/random',
-        headers={
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0'
-        }
-    ).text
-    adv = json.loads(adv_json)['text']
-    return f'{random.choice(["Охуенный", "Хуёвый"])} блять совет: {adv}'
+async def advice(cm: util.CommandMessage):
+    try:
+        adv_json = requests.get(
+            'http://fucking-great-advice.ru/api/random',
+            headers={
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0'
+            }
+        ).text
+        adv = json.loads(adv_json)['text']
+    except:
+        await cm.int_cur.reply("Advice API down")
+        util.log_fail(util.get_log("adv"), "API down")
+
+    await cm.int_cur.reply(f'{random.choice(["Охуенный", "Хуёвый"])} блять совет{" " + cm.arg if cm.arg else ""}: {adv}')
 
 
 handlers.append(
-    util.get_handler_simple_reply(
-        msg='advice',
-        ans=advice,
+    util.CommandHandler(
+        name='advice',
+        pattern=util.re_pat_starts_with('/?(adv|совет)'),
+        help_message=
+                    "Fucking great advice from fucking-great-advice.ru\n" +
+                    "Based on: github.com/Catware-Foundation/Catpy-Software/blob/main/advice.py",
         author='@yuki_the_girl',
         version=1,
-        help_message=
-            "Fucking great advice from fucking-great-advice.ru\n" +
-            "Based on: github.com/Catware-Foundation/Catpy-Software/blob/main/advice.py",
-        pattern= util.re_pat_starts_with('/?(adv|совет)')
+        handler_impl=advice,
+        is_prefix=True
     )
 )
