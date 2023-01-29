99bf103456decb6058a6815070aebac2d884a165
yuki <yuki.utk8g@gmail.com>
2023-01-29 17:53:42 +0300

    add iuliia (ru_cyr -> ru_lat)

--- commands/kb.py
+++ commands/kb.py
@@ -1,12 +1,17 @@
 import util
 
 import re
+import iuliia
 
 handlers = []
 
 
 async def on_layout(cm: util.CommandMessage):
     await cm.int_cur.reply(util.change_layout(cm.arg))
+
+
+async def on_trl(cm: util.CommandMessage):
+    await cm.int_cur.reply(iuliia.translate(cm.arg, iuliia.WIKIPEDIA))
 
 
 handlers.append(util.CommandHandler(
@@ -18,4 +23,11 @@ handlers.append(util.CommandHandler(
     is_prefix=True
 ))
 
-
+handlers.append(util.CommandHandler(
+    name='iuliia',
+    pattern=re.compile(util.re_pat_starts_with('/?(trl|translit|iuliia|трл|транслит|йуля)')),
+    help_message='wikipedia-style iuliia transliterate (привет -> privet), habr.com/post/499574',
+    author='@yuki_the_girl',
+    handler_impl=on_trl,
+    is_prefix=True
+))
