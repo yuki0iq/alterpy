422a6591a8b7b0da0d013af75f3d5f7559e398fd
yuki <yuki.utk8g@gmail.com>
2023-01-30 18:09:47 +0300

    add /test

--- commands/ping.py
+++ commands/ping.py
@@ -14,38 +14,46 @@ tzMSK4 = zoneinfo.ZoneInfo("Asia/Krasnoyarsk")
 time_format = "(%Z) %Y-%m-%d, %H:%M:%S"
 
 
-async def on_ping(cm: util.CommandMessage):
-    cur_time = datetime.datetime.now(datetime.timezone.utc)
+def on_ping_wrapper(rep: str):
+    async def on_ping(cm: util.CommandMessage):
+        cur_time = datetime.datetime.now(datetime.timezone.utc)
 
-    ping = cm.local_time - cm.time
-    handle = cur_time - cm.local_time
-    up = cur_time - start_time
+        ping = cm.local_time - cm.time
+        handle = cur_time - cm.local_time
+        up = cur_time - start_time
 
-    ping_s = util.timedelta_to_str(ping, is_short=True)
-    handle_s = util.timedelta_to_str(handle, is_short=True)
-    up_s = util.timedelta_to_str(up)
+        ping_s = util.timedelta_to_str(ping, is_short=True)
+        handle_s = util.timedelta_to_str(handle, is_short=True)
+        up_s = util.timedelta_to_str(up)
 
-    await cm.int_cur.reply(
-        f"**PONG**. Ping is {ping_s}, handled in {handle_s}\n"
-        + f"Up for {up_s}\n"
-        + f"\n"
-        + f"__Current time is__\n"
-        + f"{cur_time.astimezone(tzMSK).strftime(time_format)}\n"
-        + f"{cur_time.astimezone(tzMSK4).strftime(time_format)}\n"
-        + f"__Started at__\n"
-        + f"{start_time.astimezone(tzMSK).strftime(time_format)}\n"
-        + f"{start_time.astimezone(tzMSK4).strftime(time_format)}\n"
-    )
+        await cm.int_cur.reply(
+            f"**{rep}**. Ping is {ping_s}, handled in {handle_s}\n"
+            + f"Up for {up_s}\n"
+            + f"\n"
+            + f"__Current time is__\n"
+            + f"{cur_time.astimezone(tzMSK).strftime(time_format)}\n"
+            + f"{cur_time.astimezone(tzMSK4).strftime(time_format)}\n"
+            + f"__Started at__\n"
+            + f"{start_time.astimezone(tzMSK).strftime(time_format)}\n"
+            + f"{start_time.astimezone(tzMSK4).strftime(time_format)}\n"
+        )
+    return on_ping
 
-
-handlers.append(util.CommandHandler(
-    name='ping',
-    pattern=re.compile(util.re_pat_starts_with('/?(ping|пинг)$')),
-    help_message='Measure ping',
-    author='@yuki_the_girl',
-    handler_impl=on_ping,
-    is_elevated=False
-))
+handlers.extend(
+    util.CommandHandler(
+        name=msg,
+        pattern=re.compile(util.re_pat_starts_with(f'/?{msg}$')),
+        help_message='Measure ping',
+        author='@yuki_the_girl',
+        handler_impl=on_ping_wrapper(ans),
+        is_elevated=False
+    ) for msg, ans in [
+        ("ping", "PONG"),
+        ("пинг", "ПОНГ"),
+        ("test", "PASSED"),
+        ("тест", "ПРОЙДЕН")
+    ]
+)
 
 handlers.extend(
     util.get_handler_simple_reply(msg, ans, '@yuki_the_girl', 'Simple ping replier', pat)
