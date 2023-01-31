28d791d9959de5601ce7b751c5bce347c2bc6fdf
yuki <yuki.utk8g@gmail.com>
2023-01-31 22:11:58 +0300

    fix main.py

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ import traceback
 log = util.get_log("main")
 log.info("AlterPy")
 
+
 telethon_config = util.get_config("telethon_config.toml")
-api_id = telethon_config['api_id']
-api_hash = telethon_config['api_hash']
-bot_token = telethon_config['bot_token']
-client = telethon.TelegramClient("alterpy", api_id, api_hash)
-client.start(bot_token=bot_token)
+client = telethon.TelegramClient("alterpy", telethon_config['api_id'], telethon_config['api_hash'])
+client.start(bot_token=telethon_config['bot_token'])
 log.info("Started telethon instance")
 
-the_bot_id = int(bot_token.split(':')[0])
+the_bot_id = int(telethon_config['bot_token'].split(':')[0])
+del telethon_config
 
 
 handlers = []
