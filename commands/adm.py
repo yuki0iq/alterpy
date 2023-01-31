1d765967b4c33d7b24c3f12ffc58f6be5c5f336a
yuki <yuki.utk8g@gmail.com>
2023-01-31 16:57:20 +0300

    add /exec

---
+++ commands/adm.py
@@ -0,0 +1,28 @@
+import util
+
+import re
+import traceback
+
+handlers = []
+
+
+async def on_exec(cm: util.CommandMessage):
+    try:
+        sharg = cm.arg.replace('\n', '    \n')
+        code = '\n'.join([
+            f"import asyncio",
+            f"async def func():",
+            f"    {sharg}",
+            f"task = asyncio.get_event_loop().create_task(func())",
+            f"asyncio.wait(task)"
+        ])
+        exec(code, locals() | globals())
+    except:
+        await cm.int_cur.reply(f"```{traceback.format_exc()}```")
+        if 'code' in locals():
+            code_lines = code.split('\n')
+            lined_code = '\n'.join(f"{i+1}  {code_lines[i]}" for i in range(len(code_lines)))
+            await cm.int_cur.reply(f"While executing following code:\n```{lined_code}```")
+
+
+handlers.append(util.CommandHandler("exec", re.compile(util.re_pat_starts_with("/?(exec)")), "Execute python code", "@yuki_the_girl", on_exec, is_prefix=True, is_elevated=True))
