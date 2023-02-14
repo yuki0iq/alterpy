10780a061a43d2af8b22c9e8bd0c5a1d39ec455b
yuki <yuki.utk8g@gmail.com>
2023-02-14 15:43:13 +0300

    add more variability in kek answers

--- util.py
+++ util.py
@@ -70,6 +70,22 @@ def rand_or_null_fun(s: str, p: int, q: int, s2: str = "") -> typing.Callable[[]
     return lambda: (s if random.randint(1, q) <= p else s2)
 
 
+def weighted_random(pairs):
+    """
+    https://stackoverflow.com/a/14992648
+    """
+    total = sum(pair[0] for pair in pairs)
+    r = random.randint(1, total)
+    for (weight, value) in pairs:
+        r -= weight
+        if r <= 0:
+            return value
+
+
+def weighted_random_fun(pairs):
+    return lambda: weighted_random(pairs)
+
+
 def one_of_in(a: typing.Iterable, x: typing.Container | typing.Iterable):
     return any(map(lambda el: el in x, a))
 
