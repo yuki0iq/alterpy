cd2069087ddbd3798f3cb817567666d4ec6023d2
Yuki <yuki.utk8g@gmail.com>
2023-08-01 22:26:13 +0300

    fix localization in wttr-in command and refactor it

--- handlers/commands/wttr_in.py
+++ handlers/commands/wttr_in.py
@@ -39,48 +39,42 @@ translations = {
 LOC = utils.locale.Localizator(translations)
 
 
-def weather(lang: str) -> typing.Callable[[utils.cm.CommandMessage], typing.Awaitable[None]]:
-    async def weather_impl(cm: utils.cm.CommandMessage) -> None:
-        if not isinstance(alterpy.context.session, aiohttp.ClientSession):
-            return
+async def weather(cm: utils.cm.CommandMessage) -> None:
+    if not isinstance(alterpy.context.session, aiohttp.ClientSession):
+        return
 
-        if not cm.arg:
-            await cm.int_cur.reply(LOC.obj('no', lang))
-            return
+    if not cm.arg:
+        await cm.int_cur.reply(LOC.obj('no', cm.lang))
+        return
 
-        await cm.int_cur.reply(LOC.obj('wait', lang))
+    await cm.int_cur.reply(LOC.obj('wait', cm.lang))
 
-        words = ' '.join(cm.arg.split()).lower()
-        other_dic = {True: cm.arg} | dict([(words.startswith(w), words[len(w):]) for w in LOC.obj('in', lang)]) | dict([(words.startswith(w), '~' + words[len(w):]) for w in LOC.obj('near', lang)])
+    words = ' '.join(cm.arg.split()).lower()
+    other_dic = {True: cm.arg} | dict([(words.startswith(w), words[len(w):]) for w in LOC.obj('in', cm.lang)]) | dict([(words.startswith(w), '~' + words[len(w):]) for w in LOC.obj('near', cm.lang)])
 
-        arg = other_dic[True]
-        argp = arg.strip().replace(' ', '+')
+    arg = other_dic[True]
+    argp = arg.strip().replace(' ', '+')
 
-        error_str = '>>>   404'
-        async with alterpy.context.session.get(f'https://v1.wttr.in/{argp}?T0') as v1:
-            data_v1 = await v1.read()
+    error_str = '>>>   404'
+    async with alterpy.context.session.get(f'https://v1.wttr.in/{argp}?T0') as v1:
+        data_v1 = await v1.read()
 
-        if error_str in data_v1.decode():
-            await cm.int_cur.reply(LOC.obj('err', lang))
-            return
+    if error_str in data_v1.decode():
+        await cm.int_cur.reply(LOC.obj('err', cm.lang))
+        return
 
-        async with alterpy.context.session.get(f'https://v2.wttr.in/{argp}_lang={lang}.png') as v2:
-            data_v2 = await v2.read()
+    async with alterpy.context.session.get(f'https://v2.wttr.in/{argp}_lang={cm.lang}.png') as v2:
+        data_v2 = await v2.read()
 
-        await cm.int_cur.reply(' '.join([LOC.obj('weather', lang), utils.str.escape(cm.arg)]), data_v2)
-
-    return weather_impl
+    await cm.int_cur.reply(' '.join([LOC.obj('weather', cm.lang), utils.str.escape(cm.arg)]), data_v2)
 
 
 handler_list = [
     utils.ch.CommandHandler(
-        name=f'weather-{lang}',
-        pattern=utils.regex.cmd(command),
+        name=f'weather',
+        pattern=utils.regex.cmd(utils.regex.unite("погода", "weather")),
         help_page='wttrin',
-        handler_impl=weather(lang),
+        handler_impl=weather,
         is_prefix=True
-    ) for lang, command in [
-        ('ru', 'погода'),
-        ('en', 'weather'),
-    ]
+    )
 ]
