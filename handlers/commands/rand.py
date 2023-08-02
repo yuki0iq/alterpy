cde61909750f497c9c90500d01248336301b62f7
Yuki <yuki.utk8g@gmail.com>
2023-08-02 13:41:26 +0300

    fix localization in rand.py and make poll autoanswers lang-aware

--- handlers/commands/rand.py
+++ handlers/commands/rand.py
@@ -1,99 +1,88 @@
 import utils.cm
 import utils.ch
 import utils.regex
+import utils.locale
 
 import random
 import re
 
 handler_list = []
 
-prefs_en = [
-    "Yuki said",
-    "Our Government thinks",
-    "Cats selected",
-    "I was forced to say",
-    "Ryijik meowed",
-    "You'd like to hear"
-]
+translations = {
+    'prefs': {
+        'en': [
+            "Yuki said",
+            "Our Government thinks",
+            "Cats selected",
+            "I was forced to say",
+            "Ryijik meowed",
+            "You'd like to hear"
+        ],
+        'ru': [
+            "Звёзды говорят",
+            "Юки сказала",
+            "Правительство нашептало",
+            "Карты таро передали",
+            "Котики выбрали",
+            "Меня заставили сказать",
+            "Осинка мяукнула",
+            "Я знаю, что ты хочешь услышать",
+            "Проверяй,",
+            "Ставлю сотку,",
+            "Древние оракулы выбрали",
+            "Пусть будет",
+            "Как знать,",
+            "Рандом выбрал",
+            "Я выбрала",
+            "Сова выбрала",
+            "Разрабам альтерпая нефиг делать и они решили",
+            "Питон передаёт",
+            "Справедливый выбор —",
+            "Птичка нашептала"
+        ],
+    },
+}
 
-prefs_ru = [
-    "Звёзды говорят",
-    "Юки сказала",
-    "Правительство нашептало",
-    "Карты таро передали",
-    "Котики выбрали",
-    "Меня заставили сказать",
-    "Осинка мяукнула",
-    "Я знаю, что ты хочешь услышать",
-    "Проверяй,",
-    "Ставлю сотку,",
-    "Древние оракулы выбрали",
-    "Пусть будет",
-    "Как знать,",
-    "Рандом выбрал",
-    "Я выбрала",
-    "Сова выбрала",
-    "Разрабам альтерпая нефиг делать и они решили",
-    "Питон передаёт",
-    "Справедливый выбор —",
-    "Птичка нашептала"
-]
-
-
-async def on_prob_ru(cm: utils.cm.CommandMessage) -> None:
-    await cm.int_cur.reply(f"{random.choice(prefs_ru)} {random.randint(0, 100)}%")
-
-
-async def on_prob_en(cm: utils.cm.CommandMessage) -> None:
-    await cm.int_cur.reply(f"{random.choice(prefs_en)} {random.randint(0, 100)}%")
+LOC = utils.locale.Localizator(translations)
 
 
-async def on_choose_ru(cm: utils.cm.CommandMessage) -> None:
+async def on_prob(cm: utils.cm.CommandMessage) -> None:
+    pref = random.choice(LOC.obj('prefs', cm.lang))
+    val = random.randint(0, 100)
+    await cm.int_cur.reply(f"{pref} {val}%")
+
+
+async def on_choose(cm: utils.cm.CommandMessage) -> None:
     opts = re.split('(?i)(^|\\s)(or|или)($|\\s)', cm.arg)[::4]
-    await cm.int_cur.reply(f"{random.choice(prefs_ru)} {random.choice(opts).strip()}")
-
-
-async def on_choose_en(cm: utils.cm.CommandMessage) -> None:
-    opts = re.split('(?i)(^|\\s)(or|или)($|\\s)', cm.arg)[::4]
-    await cm.int_cur.reply(f"{random.choice(prefs_en)} {random.choice(opts).strip()}")
+    pref = random.choice(LOC.obj('prefs', cm.lang))
+    val = random.choice(opts).strip()
+    await cm.int_cur.reply(f"{pref} {val}")
 
 
 async def on_poll(cm: utils.cm.CommandMessage) -> None:
-    await cm.int_cur.reply(f"{random.choice(prefs_ru)} {utils.str.escape(random.choice(list(x.text for x in cm.media.poll().poll.answers)).strip())}")
+    pref = random.choice(LOC.obj('prefs', cm.lang))
+    choices = list(x.text for x in cm.media.poll().poll.answers)
+    val = random.choice(choices).strip()
+    await cm.int_cur.reply(f"{pref} {utils.str.escape(val)}")
 
-
-handler_list.append(utils.ch.CommandHandler(
-    name='шанс',
-    pattern=utils.regex.cmd(utils.regex.unite('инфа', 'шанс', 'вер' + utils.regex.optional('оятность'))),
-    help_page="random",
-    handler_impl=on_prob_ru
-))
 
 handler_list.append(utils.ch.CommandHandler(
     name='prob',
-    pattern=utils.regex.cmd(utils.regex.unite('prob', 'chance')),
+    pattern=utils.regex.cmd(utils.regex.unite('prob', 'chance', 'инфа', 'шанс', 'вер', 'вероятность')),
     help_page="random",
-    handler_impl=on_prob_en
-))
-
-handler_list.append(utils.ch.CommandHandler(
-    name='выбери',
-    pattern=utils.regex.cmd('выбери'),
-    help_page="random",
-    handler_impl=on_choose_ru,
-    is_prefix=True
+    handler_impl=on_prob
 ))
 
 handler_list.append(utils.ch.CommandHandler(
     name='choose',
-    pattern=utils.regex.cmd(utils.regex.unite('choose', 'select')),
+    pattern=utils.regex.cmd(utils.regex.unite('choose', 'select', 'выбери')),
     help_page="random",
-    handler_impl=on_choose_en,
+    handler_impl=on_choose,
     is_prefix=True
 ))
 
 handler_list.append(utils.ch.CommandHandler(
-    name='poll-choose-ru',
+    name='poll-choose',
     pattern=utils.regex.ignore_case(""),
     help_page="random",
     handler_impl=on_poll,
