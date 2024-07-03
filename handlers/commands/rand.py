53c25e418470308492163f57c0f3c6c6614682ee
Yulia 🥺 <44705138+yuki0iq@users.noreply.github.com>
2024-07-03 23:59:54 +0300

    Update rand.py

--- handlers/commands/rand.py
+++ handlers/commands/rand.py
@@ -61,7 +61,7 @@ async def on_choose(cm: utils.cm.CommandMessage) -> None:
 
 async def on_poll(cm: utils.cm.CommandMessage) -> None:
     pref = random.choice(LOC.obj('prefs', cm.lang))
-    choices = list(x.text for x in cm.media.poll().poll.answers)
+    choices = list(str(x.text) for x in cm.media.poll().poll.answers)
     val = random.choice(choices)
     await cm.int_cur.reply(f"{pref} {utils.str.escape(val)}")
 
